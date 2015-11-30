2846bc69f09988fec3094eb0b3acb40bccffff9f
mmikowski <mike.mikowski@gmail.com>
2015-11-30 14:51:09 -0800

    numerous refinements, style compatibility, minor bug fixes

--- jquery.carouscroll.js
+++ jquery.carouscroll.js
@@ -36,11 +36,10 @@
  *
  *     $collection.carouscroll({
  *       _action_str_   : '_create_',
- *       _content_html_ : the_html, // optional
- *       _label_list_   : [         // optional
- *         'Summary', 'Health', 'Monitor', 'Contacts',
- *         'Interfaces', 'Software', 'Hardware', 'Config'
- *       ],
+ *       _content_html_ : the_html, // required
+ *       // A label_list will be pulled from <h1> tags in content,
+ *       // e.g. 'Summary', 'Health', 'Monitor', 'Contacts',
+ *       //  'Interfaces', 'Software', 'Hardware', 'Config'
  *       _head_ds_arg_map_ : {}  // optional dragscroll options carousel
  *       _body_ds_arg_map_ : {}  // optional dragscroll options vertial
  *     });
@@ -52,8 +51,8 @@
  *
  *     $collection.carouscroll({
  *       _action_str_    : _change_,
- *       _content_html_  : new_html,
- *       _label_list_    : [ 'Flopsy', 'Mopsy', 'CottonTail', 'Peter' ]
+ *       _content_html_  : new_html
+ *       // label_list example: [ 'Flopsy', 'Mopsy', 'CottonTail', 'Peter' ]
  *     });
  *
  * Remove carouscroll instances (empties containers):
@@ -61,7 +60,6 @@
  *     $collection.carouscroll({
  *       _action_str_ : '_remove_'
  *     });
- *
  *
  * Please the carouscroll.html for a demonstration of use.
  *
@@ -121,7 +119,7 @@
         + '<div class="jqcsx-_head_inc_r_"></div>'
         + '</div>'
         + '<div class="jqcsx-_si_top_">&#xf141;</div>'
-        + '<div class="jqcsx-_box_">{_scroll_html_}</div>'
+        + '<div class="jqcsx-_box_"></div>'
         + '<div class="jqcsx-_si_btm_">&#xf141;</div>',
 
       _style_css_ : __blank
@@ -155,7 +153,6 @@
         + 'left:0;'
         + 'right:0;'
         + 'height:3rem;'
-        + 'background:#ea9999;'
         + 'color:#600;'
         + '}'
 
@@ -190,20 +187,22 @@
         + 'left:0;'
         + 'right:0;'
         + 'text-align:center;'
-        + 'height:.5rem;'
+        + 'height:.75rem;'
         + 'line-height:.75rem;'
         + 'font-family:FontAwesome;'
-        + 'color:#ea9999;'
+        + 'color:transparent;'
         + '}'
 
         + '.jqcsx-_si_top_ { top:3rem; }'
         + '.jqcsx-_si_btm_ { bottom:0; }'
+
         + '.jqcsx-_si_btm_.jqcsx-_x_active_,'
         + '.jqcsx-_si_top_.jqcsx-_x_active_ { color:#fff; }'
+
         + '.jqcsx-_box_ {'
         + 'position:absolute;'
-        + 'top:3.5rem;'
-        + 'bottom:.5rem;'
+        + 'top:3.75rem;'
+        + 'bottom:.75rem;'
         + 'left:0;'
         + 'right:0;'
         + 'background:#fff;'
@@ -233,7 +232,7 @@
         + 'position:absolute;'
         + 'top:0;'
         + 'height:3rem;'
-        + 'line-height:3rem;'
+        + 'line-height:3.75rem;'
         + 'overflow:hidden;'
         + 'transition:all 0.2s ease;'
         + '}'
@@ -371,11 +370,11 @@
 
       scrollto_list    = state_map._scrollto_list_,
       title_count      = state_map._title_count_,
-      uniq_count       = state_map._uniq_count_,
+      h1_count         = state_map._h1_count_,
       title_offset_int = state_map._title_offset_int_,
 
       i, next_int, next_class_str,
-      scroll_num, $h1, uniq_idx, anim_ms;
+      scroll_num, $h1, h1_idx, anim_ms;
 
     state_map._is_our_scroll_ = __true;
 
@@ -394,11 +393,11 @@
       if ( next_int === configMap._title_midpoint_idx_ ) {
         scroll_num = scrollto_list[ i ];
 
-        uniq_idx = i;
-        while( uniq_idx >= uniq_count ) {
-          uniq_idx -= uniq_count;
+        h1_idx = i;
+        while( h1_idx >= h1_count ) {
+          h1_idx -= h1_count;
         }
-        $h1 = $h1_list.eq( uniq_idx );
+        $h1 = $h1_list.eq( h1_idx );
         $h1_list.removeClass( 'jqcsx-_x_select_' );
         $h1.addClass( 'jqcsx-_x_select_' );
 
@@ -434,18 +433,18 @@
   // BEGIN syncToScroll
   syncToScroll = function ( state_map ) {
     var
-      jquery_map       = state_map._jquery_map_,
-      scrollto_list    = state_map._scrollto_list_,
-      uniq_count       = state_map._uniq_count_,
-      title_idx        = state_map._title_idx_,
-      scroll_num       = jquery_map._$scroll_box_.scrollTop(),
-      min_delta_num    = 999999,
+      jquery_map    = state_map._jquery_map_,
+      scrollto_list = state_map._scrollto_list_,
+      h1_count      = state_map._h1_count_,
+      title_idx     = state_map._title_idx_,
+      scroll_num    = jquery_map._$scroll_box_.scrollTop(),
+      min_delta_num = 999999,
 
       pos_num, delta_num, prelim_idx, i;
     if ( state_map._is_our_scroll_ ) { return; }
 
     // find closest title position
-    for ( i = __0; i < uniq_count; i++ ) {
+    for ( i = __0; i < h1_count; i++ ) {
       pos_num = scrollto_list[ i ];
       if ( pos_num === __undef ) { break; }
 
@@ -477,45 +476,80 @@
   addContent = function ( arg_map ) {
     var
       jquery_map, content_html, label_list,
-      $scroll_box, $h1_list, h1_count,
-      i, scrollto_list, vert_offset_num, scrolltop_num,
-      title_count, uniq_count, norm_label_list, norm_scrollto_list,
+
+      $scroll_box, $h1_list, h1_count, $h1, h1_ht_num, i,
+
+      scrollto_list, vert_offset_num, scrolltop_num,
+
+      scroll_ht_num, box_ht_num, end_ht_num,
+      delta_ht_num, max_scroll_num, min_scroll_num,
+
+      norm_title_count,   norm_label_list,
+      norm_scrollto_list, title_count,
+
       title_html_list, $title_list
       ;
 
     jquery_map   = arg_map._jquery_map_;
     content_html = arg_map._content_html_;
-    label_list   = arg_map._label_list_;
 
-    if ( ! ( jquery_map && content_html && label_list ) ) { return false; }
+    if ( ! ( jquery_map && content_html ) ) { return false; }
 
     // Fill the scroll box and get the h1 tags
-    $scroll_box      = jquery_map._$scroll_box_;
-    $scroll_box.html( content_html );
+    $scroll_box = jquery_map._$scroll_box_;
+    $scroll_box.html(
+      content_html
+      + '<div class="jqcsx-_box_spacer_"></div>'
+    );
+
+    // Loop through all h1 tags
     $h1_list = $scroll_box.children( 'h1' );
     h1_count = $h1_list.length;
+    label_list    = [];
+    scrollto_list = [];
+    for ( i = __0; i < h1_count; i++ ) {
+      $h1 = $h1_list.eq( i );
 
-    // Populate scrollto_list with scrolltop numbers for each h1 tag
-    scrollto_list    = [];
-    vert_offset_num = $h1_list.eq( __0 ).offset().top;
-    scrollto_list[ __0 ] = __0;
-    for ( i = __1; i < h1_count; i++ ) {
-      scrolltop_num = $h1_list.eq( i ).offset().top - vert_offset_num;
+      // Build label list
+      label_list.push( $h1.text() );
+
+      // Build scroll-top list for h1 tags. 
+      //   Use the offset().top of the first h1 as our baseline y=0
+      if ( i === __0 ) {
+        vert_offset_num = $h1.offset().top;
+        h1_ht_num       = $h1.height();
+      }
+      scrolltop_num = $h1.offset().top - vert_offset_num;
       scrollto_list.push( scrolltop_num );
     }
 
+    // Adjust spacer div at end to allow last title to reach top
+    scroll_ht_num  = $scroll_box.prop('scrollHeight');
+    box_ht_num     = $scroll_box.height();
+    end_ht_num     = scroll_ht_num - scrollto_list[ h1_count - 1 ];
+    delta_ht_num   = end_ht_num - box_ht_num;
+
+    // TODO this is a hack
+    max_scroll_num = scroll_ht_num - box_ht_num - vert_offset_num - h1_ht_num;
+    min_scroll_num = h1_ht_num;
+
+    if (  delta_ht_num > __0 ) {
+      $scroll_box.find( '.jqcsx-_box_spacer_' ).height( delta_ht_num );
+      scroll_ht_num += delta_ht_num;
+    }
+
     // Normalize label list and scrollto_list to have at least 7 elements
-    title_count        = __0;
-    uniq_count         = label_list.length;
+    norm_title_count   = __0;
     norm_label_list    = [];
     norm_scrollto_list = [];
-    while ( title_count < configMap._title_total_count_ ) {
+    while ( norm_title_count < configMap._title_total_count_ ) {
       norm_label_list     = norm_label_list.concat( label_list );
       norm_scrollto_list  = norm_scrollto_list.concat( scrollto_list );
-      title_count         = norm_label_list.length;
+      norm_title_count    = norm_label_list.length;
     }
     label_list    = norm_label_list;
     scrollto_list = norm_scrollto_list;
+    title_count   = norm_title_count;
 
     // Populate titles. Those greater than 6 are hidden
     title_html_list = [];
@@ -529,14 +563,16 @@
     // Update state map
     return {
       _$h1_list_        : $h1_list,
-      _$title_list_     : $title_list,
-      _scrollto_list_   : scrollto_list,
       _label_list_      : label_list,
-      _min_scrollnum_   : vert_offset_num,
-      _max_scrollnum_   : $scroll_box.prop('scrollHeight')
-        - $scroll_box.height() - vert_offset_num,
-      _title_count_     : title_count,
-      _uniq_count_      : uniq_count
+      _h1_count_        : h1_count,
+
+      _max_scrollnum_   : max_scroll_num,
+      _min_scrollnum_   : min_scroll_num,
+
+      _scrollto_list_   : scrollto_list,
+
+      _$title_list_     : $title_list,
+      _title_count_     : title_count
     };
   };
   // END addContent
@@ -547,8 +583,8 @@
 
     // Arg check
     if ( $one_div.length !== __1 ) { return; }
-    if ( ! ( arg_map._content_html_ && arg_map._label_list_ ) ) {
-      console.error( '_content_list__and__title_list__are_required' );
+    if ( ! arg_map._content_html_ ) {
+      console.error( '_content_html_is_required_' );
       return;
     }
 
@@ -559,22 +595,26 @@
     // Add content and set title
     content_map = addContent({
       _jquery_map_   : jquery_map,
-      _content_html_ : arg_map._content_html_ || __blank,
-      _label_list_   : arg_map._label_list_   || []
+      _content_html_ : arg_map._content_html_ || __blank
     });
 
+    // Remove previous carouscroll if present
+    if ( in_state_map ) {
+      $one_div.data( 'jqcsx-state-map', __undef );
+    }
+
     // Begin create instance
-    // TODO do something if in_state_map is already defined
     jquery_map._$h1_list_    = content_map._$h1_list_;
     jquery_map._$title_list_ = content_map._$title_list_;
     state_map = {
       _jquery_map_       : jquery_map,
       _scrollto_list_    : content_map._scrollto_list_,
       _is_our_scroll_    : __false,
+      _$h1_list_         : content_map.$h1_list,
+      _h1_count_         : content_map._h1_count_,
 
       _label_list_       : content_map._label_list_,
       _title_count_      : content_map._title_count_,
-      _uniq_count_       : content_map._uniq_count_,
       _title_idx_        : __0,
       _title_offset_int_ : __0,
 
